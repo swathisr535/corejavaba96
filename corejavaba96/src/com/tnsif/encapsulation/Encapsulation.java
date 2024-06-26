dbc7bd461c027c4ec4eb1cce22b26bbe6dff75f5
Sagar S R <Sagar S R@LAPTOP-C4B6L6NB>
2024-06-26 11:44:15 +0530

    first commit

---
+++ corejavaba96/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,11 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialNum;
+	String name;
+	int age;
+	
+	void show() {
+		System.out.println("serialnum: "+serialNum+"\t Name: "+name+"\t Age:"+age);
+	}
+}
---
+++ corejavaba96/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -0,0 +1,14 @@
+package com.tnsif.encapsulation;
+
+public class EncapsulationDemo {
+	
+	public static void main(String[] args) {
+		
+		Encapsulation ob = new Encapsulation();
+		ob.serialNum=70;
+		ob.name="veena";
+		ob.age=20;
+	    ob.show();
+	}
+
+}
