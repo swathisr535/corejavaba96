ee5aae909fabb2420605baf92b57d04b1d5e321f
Sagar S R <Sagar S R@LAPTOP-C4B6L6NB>
2024-06-26 12:33:02 +0530

    first commit

--- corejavaba96/src/com/tnsif/encapsulation/Encapsulation.java
+++ corejavaba96/src/com/tnsif/encapsulation/Encapsulation.java
@@ -1,11 +1,38 @@
 package com.tnsif.encapsulation;
 
 public class Encapsulation {
-	int serialNum;
-	String name;
-	int age;
+	private int serialNum;
+	private String name;
+	private int age;
 	
-	void show() {
-		System.out.println("serialnum: "+serialNum+"\t Name: "+name+"\t Age:"+age);
+	public int getserialNum() {
+		return serialNum;
 	}
+	public void setserialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+	public String getname() {
+		return name;
+	}
+	public void setname(String name) {
+		this.name = name;
+	}
+	public int getage() {
+		return age;
+	}
+	public void setage(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Encapsulation [serialNum=" + serialNum + ", name=" + name + ", age=" + age + "]";
+	}
+	
+//	void show() {
+//		System.out.println("serialnum: "+serialNum+"\t Name: "+name+"\t Age:"+age);
+//	}
+
+	
+	
+	
 }
--- corejavaba96/src/com/tnsif/encapsulation/EncapsulationDemo.java
+++ corejavaba96/src/com/tnsif/encapsulation/EncapsulationDemo.java
@@ -5,10 +5,21 @@ public class EncapsulationDemo {
 	public static void main(String[] args) {
 		
 		Encapsulation ob = new Encapsulation();
-		ob.serialNum=70;
-		ob.name="veena";
-		ob.age=20;
-	    ob.show();
+//		ob.serialNum=70;
+//		ob.name="veena";
+//		ob.age=20;
+//	    ob.show();
+		
+		ob.setserialNum(50);
+		ob.setname("sagar");
+		ob.setage(23);
+		
+		ob.getserialNum();
+		ob.getname();
+		ob.getage();
+		
+		System.out.println(ob);
+		
 	}
 
 }
